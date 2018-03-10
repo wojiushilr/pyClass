024f3fe1de4c8a6c7738e96bd308a9dc6be1ab1c
wojiushilr <32765794+wojiushilr@users.noreply.github.com>
2018-03-10 20:51:15 +0900

    update

--- Leetcode_test/10_dict.py
+++ Leetcode_test/10_dict.py
@@ -17,3 +17,25 @@ while True:
 
 
 
+
+
+
+l = 'sam 111'
+print(l.split())
+
+
+a=" \rzha ng\n\t "
+print(a.split())
+
+
+str = "*****this is string example....wow!!!*****"
+print (str.strip( '*' ))
+
+str = "this is string example....wow!!!"
+print (str.split( ))
+print (str.split('i',1))
+print (str.split('w'))
+# str -- 分隔符，默认为所有的空字符，包括空格、换行(\n)、制表符(\t)等。
+# num -- 分割次数。
+
+
